8bbe6c3450ee70a3d151de40f777283a195985e9
cpfaff <ctpfaff@arcor.de>
2013-06-12 20:03:41 +0200

    Several improvements
    o Improves description adds testthat
    o Adds news file
    o Adds run all test files
    o Improves the eml template

--- reml/R/eml.read.r
+++ reml/R/eml.read.r
@@ -12,106 +12,183 @@ eml.read <- function(eml_url, eml_path) {
   if (!missing(eml_path)) eml_url = eml_path
   eml = xmlParse(eml_url)
 
-  template = list( 
+  dataset = list ( 
+    # DatasetType list
 
+    # Resource Group
+    alternateIdentifier = "//dataset/alternateIdentifier",  # (dashed)
+    shortName = "//dataset/shortName", # (dashed)
     title = "//dataset/title", 
+    creator = "//dataset/creator", # (module responsible party)
+    metadataProvider = "//dataset/metadataProvider", # (module responsible party)
+    associatedParty = "//dataset/associatedParty", # (module responsible party)
+    role = "//dataset/associatedParty/role", # (responsible party but pulled out already) 
+    pubDate = "//dataset/pubDate", # (dashed) 
+    language = "//dataset/language", # (dashed)  
+    series = "//dataset/series", # (dashed) 
+    abstract = "//dataset/abstract", # (dashed, module TextType) 
 
-    creators = list(
-      salutation = "//creator/individualName/salutation",
-      givenName = "//creator/individualName/givenName",
-      surName = "//creator/individualName/surName",
-      electronicMailAddress = "//creator/electronicMailAddress",
-      organizationName = "//creator/organizationName",
-      deliveryPoint = "//creator/address/deliveryPoint",
-      city = "//creator/address/city",
-      administrativeArea = "//creator/address/administrativeArea",
-      postalCode = "//creator/address/postalCode",
-      country = "//creator/address/postalCode",
-      phone = "//creator/phone[@phonetype='voice']",
-      fax = "//creator/phone[@phonetype='fax']",
-      electronicMailAddress = "//creator/electronicMailAddress",
-      onlineUrl = "//creator/onlineUrl"),
+    keywordSet = "//dataset/keywordSet", # (dashed, node contains 1..n keywords or keyword thesaurusname) 
+      keyword = "//dataset/keywordSet/keyword",
+      keywordThesaurus = "//dataset/keywordSet/keywordThesaurus",
+    additionalInfo = "//dataset/additionalInfo", # (dashed, module TextType) 
+    intellectualRights = "//dataset/intellectualRights", # (dashed, module TextType)
+    distribution = "//dataset/distribution", # (dashed, module DistributionType) 
+    coverage = "//dataset/coverage", # (dashed, module Coverage) 
 
-      associatedParty = list(
-        references = "//dataset/associatedParty/references", 
-        role = "//dataset/associatedParty/role"),
-
-      abstract = "//dataset/abstract",  
-
-      keywords = "//keywordSet/keyword",  
-
-      contacts = list( 
-        salutation = "//contact/individualName/salutation",
-        givenName = "//contact/individualName/givenName",
-        surName = "//contact/individualName/surName",
-        electronicMailAddress = "//contact/electronicMailAddress",
-        organizationName = "//contact/organizationName",
-        deliveryPoint = "//contact/address/deliveryPoint",
-        city = "//contact/address/city",
-        administrativeArea = "//contact/address/administrativeArea",
-        postalCode = "//contact/address/postalCode",
-        country = "//contact/address/postalCode",
-        phone = "//contact/phone[@phonetype='voice']",
-        fax = "//contact/phone[@phonetype='fax']",
-        electronicMailAddress = "//contact/electronicMailAddress",
-        onlineUrl = "//contact/onlineUrl"),
-
-      samplingDescription = "//dataset//samplingDescription",  
-
-      intellectualRights = "//dataset/intellectualRights", 
-
-      # coverage  
-
-      geographicCoverage = list(
-        geographicDescription = "//geographicCoverage/geographicDescription",
-        boundingCoordinates = c(westBoundingCoordinate = "//geographicCoverage/boundingCoordinates/westBoundingCoordinate",
-          eastBoundingCoordinate = "//geographicCoverage/boundingCoordinates/eastBoundingCoordinate",
-          northBoundingCoordinate = "//geographicCoverage/boundingCoordinates/northBoundingCoordinate",
-          southBoundingCoordinate = "//geographicCoverage/boundingCoordinates/southBoundingCoordinate")),  
-
-      temporalCoverage = list(
-        beginDate = "//temporalCoverage//beginDate", 
-        endDate = "//temporalCoverage//endDate"
-      ),
+    purpose = "//dataset/purpose", # (dashed, module TextType) 
+   
+    # Maintenance Type 
+    maintenance = "//dataset/maintenance", # (dashed, node contains description, update frequency, histor)
+      description = "//dataset//maintenance/description", # (dashed, module TextType) 
+      maintenanceUpdateFrequency = "//dataset/maintenance/maintenanceupdatefrequency", # (dashed) 
+      changeHistory = "//dataset/maintenance/changeHistory", # (dashed, node contains scope oldValue, change date, and comment) 
+        changeScope = "//dataset/maintenance/changeHistory/changeScope",  
+        oldValue = "//dataset/maintenance/changeHistory/changeDate",
+        changeDate = "//dataset/maintenance/changeHistory/changeDate", 
+        comment = "//dataset/maintenance/changeHistory/comment", # (dashed) 
+    
+      contact = "//dataest/contact", # (module ResponsibleParty)
+      publisher = "//dataset/publisher", # (dashed, module ResponsibleParty) 
+      pubPlace = "//dataset/pubPlace", # (dashed) 
+      methods = "//dataset/methods", # (dashed, module MethodType) 
+      project = "//dataest/project", # (dashed, module ResearchProjectType) 
       
-      taxonomicSystem = list( 
-        title = "//coverage//taxonomicCoverage//title", 
-        creator = "//coverage//taxonomicCoverage//creator",
-        publicationDate = "//coverage//taxonomicCoverage//pubDate", 
-        journal = "//coverage//taxonomicCoverage//journal",
-        volume = "//coverage//taxonomicCoverage//volume",
-        issue = "//coverage//taxonomicCoverage//issue",
-        pageRange = "//coverage//taxonomicCoverage//pageRange",
-        publisher = "//coverage//taxonomicCoverage//publisher", 
-        identifierName = "//coverage//taxonomicCoverage//identifierName",
-        taxonomicProcedures = "//coverage//taxonomicCoverage//taxonomicProcedures"
-      ),  
-
-      taxonomicClassification = "//taxonomicClassification"
-    ) 
-
-    out = rapply(template, function(x) xmlNodesValue(path=x, doc=eml), how="replace") 
-
-    out$creators = as.data.frame(out$creators, stringsAsFactors=F) 
+      # 0..n 
+      dataTable = "//dataset/dataTable", # (module DataTableType)
+      spatialRaster = "//dataset/spatialRaster", # (module SpatialRasterType) 
+      spatialVector = "//dataset/spatialVector", # (module SpatialVectorType) 
+      storedProcedure = "//dataset/storedProcedure", #(module StoreProcedureType) 
+      view = "//dataset/view", # (module ViewType) 
+      otherEntity = "//dataset/otherEntity", # (module ViewType) 
      
-    out$contacts = as.data.frame(out$contacts, stringsAsFactors=F) 
+      # Reverence Group  
+      references = "//dataset/references"
+    )
 
-    attributeList = getNodeSet(eml, path="//attributeList/attribute")
 
-    column_template = list(header = "./attributeLabel", description = "./attributeDefinition", unit = ".//unit") 
+  citation = list(
+    # CitationType list 
+   
+    # Resource Group 
+    alternateIdentifier = "//citation//alternateIdentifier", # (0..n)  
 
-    columns = lapply(column_template, function(c) { 
-      sapply(attributeList, function(d) {
-        xmlNodesValue(doc=d, path=c)
-      })
-    })
+    shortName = "//citation/shortName", # (dashed) 
+    title = "//citation/title", # (1..n) 
+    creator = "//citation/creator", # (1..n, module ResponsibleParty)
+    metadataProvider = "//citation/metadataProvider", # (dashed, 0..n, module ResponsibleParty) 
+    associatedParty = "//citation/associatedParty", # (dashed, 0..n, module ResponsibleParty) 
+      role = "//citation/associatedParty/role",
+    pubDate = "//citation/pubDate", # (dashed) 
+    language = "//citation/language", # (dashed) 
+    series = "//citation/series", # (dashed) 
+    abstract = "//citation/abstract", # (dashed, module TextType) 
+    keywordSet = "//citation/keywordSet", # (dashed, 0..n, conrains: keyword 1.n and kekywordThesaurus) 
+      keyword = "//citation/keywordSet/keyword", # (1..n) 
+      keywordThesaurus = "//citation/keywordSet/keywordThesaurus", # (dashed) 
+    additionalInfo = "//citation/additionalInfo", # (dashed, 0..n, module TextType) 
+    intellecutalRights = "//citation/"
+  )
 
-    columns = as.data.frame(columns, stringsAsFactors=F)
+  # template = list( 
 
-    if (nrow(columns)) {
-      columns$unit = as.character(columns$unit)
-      columns$unit[is.na(columns$unit) | columns$unit == "dimensionless"] = "dimensionless"
-      out$columns = columns
-    }
+    # title = "//dataset/title", 
+
+    # creators = list(
+      # salutation = "//creator/individualName/salutation",
+      # givenName = "//creator/individualName/givenName",
+      # surName = "//creator/individualName/surName",
+      # electronicMailAddress = "//creator/electronicMailAddress",
+      # organizationName = "//creator/organizationName",
+      # deliveryPoint = "//creator/address/deliveryPoint",
+      # city = "//creator/address/city",
+      # administrativeArea = "//creator/address/administrativeArea",
+      # postalCode = "//creator/address/postalCode",
+      # country = "//creator/address/postalCode",
+      # phone = "//creator/phone[@phonetype='voice']",
+      # fax = "//creator/phone[@phonetype='fax']",
+      # electronicMailAddress = "//creator/electronicMailAddress",
+      # onlineUrl = "//creator/onlineUrl"),
+
+      # associatedParty = list(
+        # references = "//dataset/associatedParty/references", 
+        # role = "//dataset/associatedParty/role"),
+
+      # abstract = "//dataset/abstract",  
+
+      # keywords = "//keywordSet/keyword",  
+
+      # contacts = list( 
+        # salutation = "//contact/individualName/salutation",
+        # givenName = "//contact/individualName/givenName",
+        # surName = "//contact/individualName/surName",
+        # electronicMailAddress = "//contact/electronicMailAddress",
+        # organizationName = "//contact/organizationName",
+        # deliveryPoint = "//contact/address/deliveryPoint",
+        # city = "//contact/address/city",
+        # administrativeArea = "//contact/address/administrativeArea",
+        # postalCode = "//contact/address/postalCode",
+        # country = "//contact/address/postalCode",
+        # phone = "//contact/phone[@phonetype='voice']",
+        # fax = "//contact/phone[@phonetype='fax']",
+        # electronicMailAddress = "//contact/electronicMailAddress",
+        # onlineUrl = "//contact/onlineUrl"),
+
+      # samplingDescription = "//dataset//samplingDescription",  
+
+      # intellectualRights = "//dataset/intellectualRights", 
+
+      # # coverage  
+
+      # geographicCoverage = list(
+        # geographicDescription = "//geographicCoverage/geographicDescription",
+        # boundingCoordinates = c(westBoundingCoordinate = "//geographicCoverage/boundingCoordinates/westBoundingCoordinate",
+          # eastBoundingCoordinate = "//geographicCoverage/boundingCoordinates/eastBoundingCoordinate",
+          # northBoundingCoordinate = "//geographicCoverage/boundingCoordinates/northBoundingCoordinate",
+          # southBoundingCoordinate = "//geographicCoverage/boundingCoordinates/southBoundingCoordinate")),  
+
+      # temporalCoverage = list(
+        # beginDate = "//temporalCoverage//beginDate", 
+        # endDate = "//temporalCoverage//endDate"
+      # ),
+      
+      # taxonomicSystem = list( 
+        # title = "//coverage//taxonomicCoverage//title", 
+        # creator = "//coverage//taxonomicCoverage//creator",
+        # publicationDate = "//coverage//taxonomicCoverage//pubDate", 
+        # journal = "//coverage//taxonomicCoverage//journal",
+        # volume = "//coverage//taxonomicCoverage//volume",
+        # issue = "//coverage//taxonomicCoverage//issue",
+        # pageRange = "//coverage//taxonomicCoverage//pageRange",
+        # publisher = "//coverage//taxonomicCoverage//publisher", 
+        # identifierName = "//coverage//taxonomicCoverage//identifierName",
+        # taxonomicProcedures = "//coverage//taxonomicCoverage//taxonomicProcedures"
+      # ),  
+
+      # taxonomicClassification = "//taxonomicClassification"
+    # ) 
+
+    out = rapply(dataset, function(x) xmlNodesValue(path=x, doc=eml), how="replace") 
+
+    # out$creators = as.data.frame(out$creators, stringsAsFactors=F) 
+    # out$contacts = as.data.frame(out$contacts, stringsAsFactors=F) 
+
+    # attributeList = getNodeSet(eml, path="//attributeList/attribute")
+
+    # column_template = list(header = "./attributeLabel", description = "./attributeDefinition", unit = ".//unit") 
+
+    # columns = lapply(column_template, function(c) { 
+      # sapply(attributeList, function(d) {
+        # xmlNodesValue(doc=d, path=c)
+      # })
+    # })
+
+    # columns = as.data.frame(columns, stringsAsFactors=F)
+
+    # if (nrow(columns)) {
+      # columns$unit = as.character(columns$unit)
+      # columns$unit[is.na(columns$unit) | columns$unit == "dimensionless"] = "dimensionless"
+      # out$columns = columns
+    # }
     return(out)
 }
